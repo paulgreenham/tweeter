23d67143f9b5ed6dd5defef1ca06dbb29a3b7304
Paul Greenham <paul.greenham@gmail.com>
2019-03-20 11:32:18 +0200

    add completed main.js file for fully functioning tweeter app

--- main.js
+++ main.js
@@ -2,3 +2,23 @@ const tweeter = Tweeter()
 const renderer = Renderer()
 
 renderer.renderPosts(tweeter.getPosts())
+
+$("#post").on("click", function(){
+    tweeter.addPost($("#input").val())
+    renderer.renderPosts(tweeter.getPosts())
+})
+
+$("#posts").on("click", ".delete", function(){
+    tweeter.removePost($(this).closest(".post").data().id)
+    renderer.renderPosts(tweeter.getPosts())
+})
+
+$('#posts').on("click", ".comment-button", function(){
+    tweeter.addComment($(this).closest("div").find("input").val(), $(this).closest(".post").data().id)
+    renderer.renderPosts(tweeter.getPosts())
+})
+
+$("#posts").on("click", ".delete-comment", function(){
+    tweeter.removeComment($(this).closest(".post").data().id, $(this).closest(".comments").data().id)
+    renderer.renderPosts(tweeter.getPosts())
+})
--- render.js
+++ render.js
@@ -4,15 +4,30 @@ const Renderer = function(){
     const renderPosts = function(posts){
         $("#posts").empty()
         for(let post of posts){
-            $("#posts").append(`<div class="post" data-id="${post.id}"><div class="post-text">${post.text}</div>${renderComments(post.comments)}</div>`)
+            $("#posts").append(
+                `<div class="post" data-id="${post.id}">
+                    <p class="post-text">${post.text}</p>
+                    ${renderComments(post.comments)}
+                    <button class="delete">Delete Post</button>
+                </div>`
+            )
         }
     }
 
     const renderComments = function(comments){
         let commentStr = ""
         for(let comment of comments){
-            commentStr += `<div class="comments" data-id="${comment.id}">${comment.text}</div>`
+            commentStr += 
+                `<div class="comments" data-id="${comment.id}">
+                    <span class="delete-comment">X</span>
+                    <span>${comment.text}</span>
+                </div>`
         }
+        commentStr +=
+            `<div>
+                <input type="text" placeholder="Write a comment" class="comments">
+                <button class="comment-button">Comment</button>
+            </div>`
         return commentStr
     }
 
